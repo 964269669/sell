a95630d06bfbddffbfc289bfed5f14565738b326
wk <964269669@qq.com>
2017-11-19 23:17:09 +0800

    header组件

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">内容</div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from 'components/header/Header.vue'
 
+const ERR_OK = 0
+
 export default {
-  name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this.$http.get('api/seller').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.seller = response.data
+        console.log(this.seller)
+      }
+    })
+  },
   components: {
     'v-header': Header
   }
@@ -22,13 +43,22 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
+
   #app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
-        text-align: center;
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.router-link-active
+            color: rgb(240, 20, 20)
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import goods from 'components/goods/Goods.vue'
+import seller from 'components/seller/Seller.vue'
+import ratings from 'components/ratings/Ratings.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/goods',
+      component: goods
+    },
+    {
+      path: '/ratings',
+      component: ratings
+    },
+    {
+      path: '/seller',
+      component: seller
     }
   ]
 })
