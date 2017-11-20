27e73767f460a8f2509290aa1b2c37717ef1dd70
wk <964269669@qq.com>
2017-11-20 22:11:40 +0800

    header组件完成

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>goods罪案</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +10,18 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  
+  .goods
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 46px;
+    width: 100%;
+    overflow: hidden;
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px;
+      background: #f3f5f7;
+    .foods-wrapper
+      flex: 1
+
 </style>
