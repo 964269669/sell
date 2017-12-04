2a7cb8cab56c9a0ba13e581664bcd37ec3462c9b
964269669 <964269669@qq.com>
2017-12-04 17:11:16 +0800

    ratingselect组件

--- src/components/food/Food.vue
+++ src/components/food/Food.vue
@@ -17,13 +17,24 @@
           <div class="price">
             <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+          </transition>
         </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
+        <!-- split组件 -->
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <transition name="fade">
-          <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
-        </transition>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -33,6 +44,12 @@
   import Vue from 'vue'
   import BScroll from 'better-scroll'
   import cartcontrol from 'components/cartcontrol/Cartcontrol'
+  import split from 'components/split/Split'
+  import ratingselect from 'components/ratingselect/Ratingselect'
+
+  // const POSITIVE = 0
+  // const NEGATIVE = 1
+  const ALL = 2
 
   export default {
     props: {
@@ -42,13 +59,22 @@
     },
     data() {
       return {
-        showFlag: false
-
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show() {
         this.showFlag = true
+        // 每次展示的时候初始化评论类型
+        this.selectType = ALL
+        this.onlyContent = true
         // 页面展示的时候初始化BScroll
         this.$nextTick(() => {
           if (!this.scroll) {
@@ -71,8 +97,21 @@
         Vue.set(this.food, 'count', 1)
       }
     },
+    created() {
+      // 接受ratingselect中发送的数据
+      this.$root.eventHub.$on('ratingType.select', (type) => {
+        this.selectType = type
+      })
+
+      // 接受ratingselect中发送的数据
+      this.$root.eventHub.$on('content.toggle', (onlyContent) => {
+        this.onlyContent = onlyContent
+      })
+    },
     components: {
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
     }
   }
 </script>
@@ -114,6 +153,7 @@
           font-size: 20px;
           color: #fff;
     .content
+      position: relative;
       padding: 18px;
       .title
         line-height: 14px;      
@@ -124,6 +164,7 @@
       .detail
         margin-bottom: 18px;
         line-height: 10px;
+        height: 10px;
         font-size: 0;
         .sell-count,.rating
           font-size: 10px;
@@ -141,27 +182,46 @@
           text-decoration: line-through;
           font-size: 10px;
           color: rgb(147, 153, 159)
-    .cartcontrol-wrapper
-      position: absolute;
-      right: 12px;
-      bottom: 12px;
-    .buy
-      position: absolute;
-      right: 18px;
-      bottom: 18px;
-      z-index: 10;
-      height: 24px;
-      line-height: 24px;
-      padding: 0 12px;
-      box-sizing: border-box;
-      font-size: 10px;
-      border-radius: 12px
-      color: #fff;
-      background: rgb(0, 160, 220);
-      opacity: 1;
-      &.fade-enter-active,&.fade-leave-active
-        transition: all 0.2s
+      .cartcontrol-wrapper
+        position: absolute;
+        right: 12px;
+        bottom: 12px;
+      .buy
+        position: absolute;
+        right: 18px;
+        bottom: 18px;
+        z-index: 10;
+        height: 24px;
+        line-height: 24px;
+        padding: 0 12px;
+        box-sizing: border-box;
+        font-size: 10px;
+        border-radius: 12px
+        color: #fff;
+        background: rgb(0, 160, 220);
         opacity: 1;
-      &.fade-enter,&.fade-leave-to
-        opacity: 0;
+        &.fade-enter-active,&.fade-leave-active
+          transition: all 0.2s
+          opacity: 1;
+        &.fade-enter,&.fade-leave-to
+          opacity: 0;
+    .info
+      padding: 18px;
+      .title
+        line-height: 14px;
+        margin-bottom: 6px;
+        font-size: 14px;
+        color: rgb(7, 17,27);
+      .text
+        line-height: 24px;
+        font-size: 12px;
+        padding: 0 8px;
+        color: rgb(77, 85,93);
+    .rating
+      padding-top: 18px;
+      .title
+        line-height: 14px;
+        margin-left: 18px;
+        font-size: 14px;
+        color: rgb(7, 17, 27)
 </style>
