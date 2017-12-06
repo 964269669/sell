86e91ce88ae44ed1de901a98d2092d5f5358e370
wk <964269669@qq.com>
2017-12-06 22:52:27 +0800

    seller组件布局

--- src/components/seller/Seller.vue
+++ src/components/seller/Seller.vue
@@ -3,29 +3,42 @@
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
-        <div class="desc">
+        <div class="desc border-1px">
           <star :size="36" :score="seller.score"></star>
-          <span class="text">{{seller.ratingCount}}</span>
-          <span class="text">{{seller.sellCount}}</span>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
         </div>
         <ul class="remark">
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>
+              <span class="stress">{{seller.minPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>商家配送</h2>
             <div class="content">
-              <span class="stress">{{seller.deliveryPrice}}</span>
+              <span class="stress">{{seller.deliveryPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>平均配送时间</h2>
             <div class="content">
-              <span class="stress">{{seller.deliveryTime}}分钟</span>
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
             </div>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,index) in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
           </li>
         </ul>
       </div>
@@ -35,6 +48,7 @@
 
 <script type="text/ecmascript-6">
   import star from 'components/star/Star'
+  import split from 'components/split/Split'
 
   export default {
     props: {
@@ -42,12 +56,78 @@
         type: Object // 从父组件App.vue中的router-view传来
       }
     },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
     components: {
-      star
+      star,
+      split
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  
+  @import '../../common/stylus/mixin.styl';
+
+  .seller
+    position: absolute;
+    top: 174px;
+    bottom: 0;
+    width: 100%;
+    left: 0;
+    overflow: hidden;
+    .overview
+      padding: 18px;
+      .title
+        font-size: 14px;
+        line-height: 14px;
+        color: rgb(7,17,27);
+        margin-bottom: 8px;
+      .desc
+        padding-bottom: 18px;
+        font-size: 0;
+        border-1px(rgba(7,17,27,0.1))
+        .star
+          display: inline-block;
+          vertical-align: top;
+          margin-right: 8px;
+        .text
+          display: inline-block;
+          line-height: 18px;
+          vertical-align: top;
+          margin-right: 12px;
+          font-size: 10px;
+          color: rgb(77,87,93);
+      .remark
+        display: flex;
+        padding-top: 18px;
+        .block
+          flex: 1;
+          text-align: center;
+          border-right:1px solid rgba(7,17,27,0.1)
+          &:last-child
+            border: none;
+          h2
+            margin-bottom: 4px;
+            line-height: 10px;
+            font-size: 10px;
+            color: rgb(147,153,159);
+          .content
+            line-height: 24px;
+            font-size: 10px;
+            color: rgb(7,17,27);
+            .stress
+              font-size: 24px;
+    .bulletin
+      padding: 18px 18px 0 18px;
+      .title
+        margin-bottom: 8px;
+        line-height: 14px;
+      .content-wrapper
+        padding: 0 12px 16px 12px;
+        border-1px(rgba(7,17,27,0.1))
+        .content
+          line-height: 24px;
+          color: rgb(240,20,20);
+          font-size: 12px;
 </style>
